63f7a27ddc35cd228939626a5bb65bd783f33eec
Unknown <spambigbox@gmail.com>
2019-02-15 11:15:44 +0100

    getting file extension from file name

--- MagicNumbers/src/com/company/Main.java
+++ MagicNumbers/src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+
+import java.io.File;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static String getFileExtension(File file) {
+        if (file == null) {
+            throw new NullPointerException("File argument was null");
+        }
+        String name = file.getName();
+        int i = name.lastIndexOf('.');
+        String ext = i > 0 ? name.substring(i + 1) : "";
+        return ext;
     }
+
+    
+
 }
