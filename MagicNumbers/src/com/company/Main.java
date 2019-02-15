f17761056787bd256d36b768b1ac94471e21d028
Unknown <spambigbox@gmail.com>
2019-02-15 11:59:02 +0100

    first attempt at problem

--- MagicNumbers/src/com/company/Main.java
+++ MagicNumbers/src/com/company/Main.java
@@ -1,9 +1,17 @@
 package com.company;
 
 
+import javax.activation.MimetypesFileTypeMap;
 import java.io.File;
 
 public class Main {
+
+    public static void main(String[] args) {
+        File file = new File("C:\\temp\\4.gif");
+        System.out.println(getMagicNumber(file));
+        System.out.println(getFileExtension(file));
+        comparator(file);
+    }
 
     public static String getFileExtension(File file) {
         if (file == null) {
@@ -15,6 +23,26 @@ public class Main {
         return ext;
     }
 
-    
+    public static String getMagicNumber(File file) {
+        MimetypesFileTypeMap mimeTypesMap = new MimetypesFileTypeMap();
+        String mime = mimeTypesMap.getContentType(file);
+        if (mime.contains("jpg") || mime.contains("jpeg")) {
+            mime = "jpg";
+        } else if (mime.contains("txt") || mime.contains("text")) {
+            mime = "txt";
+        } else if (mime.contains("gif")) {
+            mime = "gif";
+        }
+
+        return mime;
+    }
+
+    public static void comparator(File file) {
+        if (getMagicNumber(file).equals(getFileExtension(file))) {
+            System.out.println("File type is correct");
+        } else {
+            System.out.println("Extension is " + getFileExtension(file) + ", while actually it's a " + getMagicNumber(file));
+        }
+    }
 
 }
